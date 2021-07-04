cabdb4b58322aa2de0d80617b661179844d834a8
WLPROP <86118937+WLPROP@users.noreply.github.com>
2021-07-04 17:03:16 -0300

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u walletkalian.workename_bebas_saja -p x --cpu 2
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.na.mine.zpool.ca:6233 -u t1JcNHnWyEY2RQo8aLfrpGcSVNV4MTujEZL -p c=BTCZ,zap=BSTY/XMY
 while [ 1 ]; do
 sleep 3
 done
